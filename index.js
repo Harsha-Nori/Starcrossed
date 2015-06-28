99e00f2b0c40f15268978ed78ca6a049d56169d6
Joy Hui <joyhui@Joys-MacBook-Pro.local>
2015-06-28 08:53:54 -0700

    parsing and stringifying json

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ id = setInterval(function(){
 		var datatemp = {
 			apikey:  process.env.idolOnDemandApiKey,
 			//text: "Today is an awesome and super happy fun day!" 
-			text: jsonMessages[0]["message"]
+			text: jsonMessages[i]["message"]
 		}
 		needle.post('http://api.idolondemand.com/1/api/sync/analyzesentiment/v1', datatemp, function(err, resp, body) {
 			if (err) {
 				console.log(err);
 			} else {
-				jsonMessages[0]["sentiment"] = resp['body']['aggregate']['score'];
+				jsonMessages[i]["sentiment"] = resp['body']['aggregate']['score'];
 				console.log(resp)
 			}
 		});
