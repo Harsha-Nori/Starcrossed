2770d598ac545a7c964e8290ab1d814f3312f0a8
Joy Hui <joyhui@Joys-MacBook-Pro.local>
2015-06-27 22:16:51 -0700

    7

--- index.js
+++ index.js
@@ -6,30 +6,45 @@ var path = require('path')
 var http = require('http').Server(app)
 var needle = require('needle')
 var iod = require('iod-node')
+var fs = require('fs');
 // client = new iod.IODClient('http://api.idolondemand.com', process.env.idolOnDemandApiKey)
 
 previousRefID = ''
 currentRefID = ''
 updateTimer = 1000
+messagesJSON = 	'{ "employees" : [' +
+					'{ "firstName":"John" , "lastName":"Doe" },' +
+					'{ "firstName":"Anna" , "lastName":"Smith" },' +
+					'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
-// id = setInterval(function(){
+
+
+id = setInterval(function(){
 	// console.log("this worked")
-	// var data = {
-	// 	apikey:  process.env.idolOnDemandApiKey,
-	// 	file: {'file':'asdf.json','content_type':'multipart/form-data'}
-	// }
-	// needle.post('http://api.idolondemand.com/1/api/sync/storeobject/v1', data, { multipart: true, 'Content-Length': data.length }, function(err, resp, body) {
-	// 	if (err) {
-	// 		console.log(err);
-	// 	} else {
-	// 		console.log(resp);
-	// 	}
-	// })
-// }, updateTimer)
+	var path = './messages.json'
+	fs.writeFile(path, messagesJSON, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+	    console.log("The file was saved!");
+	});
+
+	var data = {
+		apikey:  process.env.idolOnDemandApiKey,
+		file: {'file':'messages.json','content_type':'multipart/form-data'}
+	}
+	needle.post('http://api.idolondemand.com/1/api/sync/storeobject/v1', data, { multipart: true, 'Content-Length': data.length }, function(err, resp, body) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(resp);
+		}
+	})
+}, updateTimer)
 
 app.get('/', function(req,res){
 	console.log('hit')
 	res.send('refid: ' + process.env.idolOnDemandApiKey)
 })
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000)
