ae4af40becc8d92e648974b5cbf260e101c220a1
Joy Hui <joyhui@Joys-MacBook-Pro.local>
2015-06-28 06:05:05 -0700

    testing sentiment

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ id = setInterval(function(){
 	for (var i = 0; i < jsonMessages.length; i++) {
 		var datatemp = {
 			apikey:  process.env.idolOnDemandApiKey,
-			text: jsonMessages[i]["message"]
+			text: "Today is an awesome and super happy fun day!"
+			//text: jsonMessages[i]["message"]
 		}
 		needle.post('http://api.idolondemand.com/1/api/sync/storeobject/v1', datatemp, function(err, resp, body) {
 			if (err) {
